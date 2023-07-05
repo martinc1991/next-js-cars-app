234813c503e023334561b16d71aed6ae0813ebc1
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-07-05 11:36:46 -0300

    Initial commit

---
+++ components/index.ts
@@ -0,0 +1,10 @@
+import CarCard from './CarCard';
+import CustomButton from './CustomButton';
+import CustomFilter from './CustomFilter';
+import Footer from './Footer';
+import Hero from './Hero';
+import NavBar from './Navbar';
+import SearchBar from './Searchbar';
+import ShowMore from './ShowMore';
+
+export { CarCard, CustomButton, CustomFilter, Footer, Hero, NavBar, SearchBar, ShowMore };
