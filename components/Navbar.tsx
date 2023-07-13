b30dadfbeb8898890db412d0363fec42129bee64
MartinC <59508264+martinc1991@users.noreply.github.com>
2023-07-13 09:07:46 -0300

    Hide Sign In button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import CustomButton from './CustomButton';
+// import CustomButton from './CustomButton';
 
 const NavBar = () => (
   <header className='w-full  absolute z-10'>
@@ -9,7 +9,7 @@ const NavBar = () => (
         <Image src='/logo.svg' alt='logo' width={118} height={18} className='object-contain' />
       </Link>
 
-      <CustomButton title='Sign in' btnType='button' containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]' />
+      {/* <CustomButton title='Sign in' btnType='button' containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]' /> */}
     </nav>
   </header>
 );
